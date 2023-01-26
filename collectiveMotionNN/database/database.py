9b2cf6854acdda98f2d7d49c1db13e5961b3d8e5
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-01-26 18:48:28 +0900

    Update database.py

--- collectiveMotionNN/database/database.py
+++ collectiveMotionNN/database/database.py
@@ -1,1 +1,10 @@
+import sqlite3
+
+def connectDB(dbname):
+    # dbname : string. The name of database file (.db) which you want to connect.
+    #          This function will create the database file if it does not exist. 
+    conn = sqlite3.connect(dbname)
+    return conn
+
+
 
