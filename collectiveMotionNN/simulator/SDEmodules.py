ef59e135258c5f0de5d9fe31c5f3f699319cb969
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-02-01 19:01:33 +0900

    Create SDEmodules.py
    The concept of code should be polished up

---
+++ collectiveMotionNN/simulator/SDEmodules.py
@@ -0,0 +1,35 @@
+import torch
+from torch import nn
+
+class singleBatchSDE(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.module_f = lambda x: x
+        
+        self.module_g = lambda x: torch.zeros_like(x)
+        
+    def f(self, t, y):
+        return self.module_f(y)
+    
+    def g(self, t, y):
+        return self.module_g(y)
+    
+    
+class multiBatchSDE(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.module_f = lambda x: x
+        
+        self.module_g = lambda x: torch.zeros_like(x)
+        
+    def f(self, t, y):
+        return self.module_f(y)
+    
+    def g(self, t, y):
+        return self.module_g(y)
+        
+        
+        
+
