767912a4f93f0a26b09d94f0a4077d42cd9d83f7
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-02-07 00:12:15 +0900

    Update module.py

--- collectiveMotionNN/NN/module.py
+++ collectiveMotionNN/NN/module.py
@@ -1,1 +1,14 @@
+import torch
+from torch import nn
+
+from collectiveMotionNN.simulator import SDEmodules
+
+class NNSDE_singleStep(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.SDEmodule = SDEmodules.singleBatchSDE()
+        
+    def forward(self, x):
+        
 
