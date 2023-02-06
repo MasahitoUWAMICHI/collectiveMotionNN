dca6d4ee61957439e2f92af1a64bbac3559aaf81
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-02-06 23:23:06 +0900

    Update SDEmodules.py

--- collectiveMotionNN/simulator/SDEmodules.py
+++ collectiveMotionNN/simulator/SDEmodules.py
@@ -1,36 +1,20 @@
 import torch
 from torch import nn
 
-class singleBatchSDE(nn.Module):
-    def __init__(self):
+class torchSDEmodule(nn.Module):
+    def __init__(self, module_f, module_g):
         super().__init__()
         
-        # module F and G should be overwritten
-        self.module_f = lambda x: x
+        self.module_f = module_f
         
-        self.module_g = lambda x: torch.zeros_like(x)
+        self.module_g = module_g
         
     def f(self, t, y):
         return self.module_f(y)
     
     def g(self, t, y):
         return self.module_g(y)
-    
-    
-class multiBatchSDE(nn.Module):
-    def __init__(self):
-        super().__init__()
-        
-        self.module_f = lambda x: x
-        
-        self.module_g = lambda x: torch.zeros_like(x)
-        
-    def f(self, t, y):
-        return self.module_f(y)
-    
-    def g(self, t, y):
-        return self.module_g(y)
-        
+
         
         
 
