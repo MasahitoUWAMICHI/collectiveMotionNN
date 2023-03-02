1f55d1349872cd13a02ef44dbafa36b75b65ee8b
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-03-02 17:30:18 +0900

    Update NeuralDEmodules.py

--- collectiveMotionNN/simulator/NeuralDEmodules.py
+++ collectiveMotionNN/simulator/NeuralDEmodules.py
@@ -1,6 +1,7 @@
-import torch
 from torch import nn
 
+
+from torchsde import BrownianInterval, sdeint
 
 class torchODEmodule(nn.Module):
     def __init__(self, module_f):
@@ -15,7 +16,7 @@ class torchODEmodule(nn.Module):
 class torchSDEmodule(nn.Module):
     def __init__(self, module_f, module_g, noise_type, sde_type):
         super().__init__()
-        
+                
         self.noise_type = noise_type
         self.sde_type = sde_type
         
