ab2e4ae34adf465db3075696c99ab1e077419ff6
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-03-02 17:22:11 +0900

    Update and rename SDEmodules.py to NeuralDEmodules.py

---
+++ collectiveMotionNN/simulator/NeuralDEmodules.py
@@ -0,0 +1,33 @@
+import torch
+from torch import nn
+
+
+class torchODEmodule(nn.Module):
+    def __init__(self, module_f):
+        super().__init__()
+                
+        self.module_f = module_f
+                
+    def f(self, t, y):
+        return self.module_f(y)
+
+
+class torchSDEmodule(nn.Module):
+    def __init__(self, module_f, module_g, noise_type, sde_type):
+        super().__init__()
+        
+        self.noise_type = noise_type
+        self.sde_type = sde_type
+        
+        self.module_f = module_f
+        
+        self.module_g = module_g
+        
+    def f(self, t, y):
+        return self.module_f(y)
+    
+    def g(self, t, y):
+        return self.module_g(y)
+
+        
+
