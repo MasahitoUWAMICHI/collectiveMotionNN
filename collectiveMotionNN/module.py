35383b0f70c35aa6533672f66a0797861a07e4cb
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-03-28 11:34:41 +0900

    Update module.py

--- collectiveMotionNN/module.py
+++ collectiveMotionNN/module.py
@@ -4,10 +4,10 @@ import graph_utils as gu
 
 
 class dynamicGODEwrapper(nn.Module):
-    def __init__(self, module_f, graph=None):
+    def __init__(self, dynamicGNDEmodule, graph=None):
         super().__init__()
 
-        self.module_f = module_f
+        self.dynamicGNDEmodule = dynamicGNDEmodule
 
         self.graph = graph
         
@@ -15,41 +15,52 @@ class dynamicGODEwrapper(nn.Module):
         self.graph, self.dynamicName = gu.make_disconnectedGraph(dynamicVariable, staticVariables, dynamicName=dynamicName)
 
     def f(self, t, y):
-        return self.module_f.f(t, y, self.graph, self.dynamicName, self.isBatched)
+        return self.dynamicGNDEmodule.f(t, y, self.graph, self.dynamicName)
 
 
 class dynamicGSDEwrapper(dynamicGODEwrapper):
-    def __init__(self, module_f, noise_type, sde_type, graph=None):
-        super().__init__(module_f, graph)
+    def __init__(self, dynamicGNDEmodule, noise_type, sde_type, graph=None):
+        super().__init__(dynamicGNDEmodule, graph)
 
         self.noise_type = noise_type
         self.sde_type = sde_type
         
     def g(self, t, y):
-        return self.module_f.g(t, y, self.graph, self.dynamicName, self.isBatched)
+        return self.dynamicGNDEmodule.g(t, y, self.graph, self.dynamicName)
 
 
 
 class dynamicGNDEmodule(nn.Module):
-    def __init__(self, edgeConditionFunc):
+    def __init__(self, calc_module, edgeConditionFunc, forceUpdate=False):
         super().__init__()
+        
+        self.calc_module = calc_module
 
         self.edgeConditionFunc = edgeConditionFunc
+        
+        self.forceUpdate = forceUpdate
 
     def edgeRefresh_execute(self, gr, dynamicVariable, dynamicName):
         gr.ndata[dynamicName] = dynamicVariable
-        gu.update_adjacency(gr, self.edgeConditionFunc)
+        gr = gu.update_adjacency_batch(gr, self.edgeConditionFunc)
+        return gr
 
-    def edgeRefresh(self, gr, dynamicVariable, dynamicName, forceUpdate=False):
-        if forceUpdate:
-            self.edgeRefresh_execute(gr, dynamicVariable, dynamicName)
+    def edgeRefresh(self, gr, dynamicVariable, dynamicName):
+        if self.forceUpdate:
+            gr = self.edgeRefresh_execute(gr, dynamicVariable, dynamicName)
+            return gr
         else:
-            if not(gu.judge_skipUpdate(gr, dynamicVariable, dynamicName)):
-                self.edgeRefresh_execute(gr, dynamicVariable, dynamicName)
+            if gu.judge_skipUpdate(gr, dynamicVariable, dynamicName):
+                return gr
+            else:
+                gr = self.edgeRefresh_execute(gr, dynamicVariable, dynamicName)
+                return gr
 
     # f and g should be updated in user-defined class
-    def f(self, t, y, gr, dynamicName, isBatched):
-        return None
+    def f(self, t, y, gr, dynamicName):
+        gr = self.edgeRefresh(gr, dynamicVariable, dynamicName)
+        return self.calc_module.f(t, y, gr, dynamicName)
 
-    def g(self, t, y, gr, dynamicName, isBatched):
-        return None
+    def g(self, t, y, gr, dynamicName):
+        gr = self.edgeRefresh(gr, dynamicVariable, dynamicName)
+        return self.calc_module.g(t, y, gr, dynamicName)
