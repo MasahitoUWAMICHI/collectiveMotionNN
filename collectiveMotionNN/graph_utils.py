2c9ef916a38209d3dd4b56967cd8b0142bd613bc
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-03-28 10:42:34 +0900

    Update graph_utils.py

--- collectiveMotionNN/graph_utils.py
+++ collectiveMotionNN/graph_utils.py
@@ -2,13 +2,14 @@ import torch
 import dgl
 
 def update_edges(g, edges):
-    g.remove_edges(g, g.edge_ids(g.edges()[0], g.edges()[1]))
-    g.add_edges(edges)
-    return None
+    g.remove_edges(g.edge_ids(g.edges()[0], g.edges()[1]))
+    g.add_edges(edges[0], edges[1])
+    return g
 
 def calc_adjacency(g, edgeConditionFunc):
     adj = edgeConditionFunc(g.ndata)
-    return torch.argwhere(adj)
+    edges = torch.argwhere(adj)
+    return (edges[:,0], edges[:,1])
 
 def update_adjacency(g, edgeConditionFunc):
     edges = calc_adjacency(g, edgeConditionFunc)
@@ -17,8 +18,8 @@ def update_adjacency(g, edgeConditionFunc):
 
 def update_adjacency_batch(bg, edgeConditionFunc):
     gs = list(dgl.unbatch(bg))
-    for i in range(len(gs)):
-        gs[i] = update_adjacency(gs[i], edgeConditionFunc)
+    for g in gs:
+        update_adjacency(g, edgeConditionFunc)
     bg = dgl.batch(gs)
     return bg
 
@@ -35,26 +36,3 @@ def make_disconnectedGraph_func(dynamicVariable, staticVariables, dynamicName):
         g.ndata[key] = staticVariables[key]
 
     return g
-
-def make_disconnectedGraph_func_batch(dynamicVariable, staticVariables, dynamicName, batchFlg):
-    Nnodes = torch.sum(batchFlg)
-    g = dgl.graph((torch.tensor([0]), torch.tensor([0])), num_nodes=Nnodes)
-    g.ndata[dynamicName] = dynamicVariable[batchFlg]
-
-    for key in staticVariables.keys():
-        g.ndata[key] = staticVariables[key][batchFlg]
-
-    return g
-
-
-def make_disconnectedGraph(dynamicVariable, staticVariables, dynamicName='y', batchID=None):
-    if batchID is None:
-        g = make_disconnectedGraph_func(dynamicVariable, staticVariables, dynamicName)
-        isBatched = False
-    else:
-        batchIDs = torch.unique(batchID)
-        g = dgl.batch([make_disconnectedGraph_func_batch(dynamicVariable, staticVariables, dynamicName, batchID==bID)\
-                        for bID in batchIDs])
-        isBatched = True
-
-    return g, dynamicName, isBatched
