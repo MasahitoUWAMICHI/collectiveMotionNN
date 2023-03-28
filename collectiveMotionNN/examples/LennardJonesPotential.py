884d6683b7a3de535792ca5d39739eb56697703b
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-03-28 15:48:47 +0900

    Update LennardJonesPotential.py

--- collectiveMotionNN/examples/LennardJonesPotential.py
+++ collectiveMotionNN/examples/LennardJonesPotential.py
@@ -4,8 +4,9 @@ from torch import nn
 
 import dgl.function as fn
 
-import collectiveMotionNN.graph_utils as gu
-import collectiveMotionNN.module as mo
+import ..utils as ut
+import ..graph_utils as gu
+import ..module as mo
 
 class LJpotential(nn.Module):
     def __init__(self, c, sigma, p=12, q=6):
@@ -19,11 +20,11 @@ class LJpotential(nn.Module):
         return 4 * self.c * (self.sigma/r)**(self.q) * ((self.sigma/r)**(self.p-self.q) - 1)
 
     def force(self, r):
-        return 4 * self.c * (self.sigma/r)**(self.q) * (self.p * (self.sigma/r)**(self.p-self.q) - self.q) / r
+        return 4 * self.c * (self.sigma/r)**(self.q) * ((self.p * (self.sigma/r)**(self.p-self.q)) - self.q) / r
     
 
 class interactionModule(nn.Module):
-    def __init__(self, d, c, sigma, p=12, q=6, periodic=None):
+    def __init__(self, d, c, sigma, p=12, q=6, periodic=None, dynamicName=None, messageName=None, aggregateName=None):
         self.d = d
         self.c = c
         self.sigma = sigma
@@ -38,18 +39,34 @@ class interactionModule(nn.Module):
             self.periodic = torch.tensor(periodic)
         else:
             self.periodic = periodic
+            
+        self.dynamicName = ut.variableInitializer(dynamicName, 'y')
         
+        self.messageName = ut.variableInitializer(messageName, 'm')
+
+        self.aggregateName = ut.variableInitializer(aggregateName, 'v')
+        
+    def set_dynamicName(self, dynamicName):
+        self.dynamicName = ut.variableInitializer(dynamicName, self.dynamicName)
+
+    def set_messageName(self, messageName):
+        self.messageName = ut.variableInitializer(messageName, self.messageName)
+
+    def set_aggregateName(self, aggregateName):
+        self.aggregateName = ut.variableInitializer(aggregateName, self.aggregateName)
         
     def calc_message(self, edges):
-        dr = calc_dr(edges.dst['x'], edges.src['x'])
+        dr = calc_dr(edges.dst[self.dynamicName], edges.src[self.dynamicName])
 
         abs_dr = torch.norm(dr, dim=-1, keepdim=True)
         unit_dr = nn.functional.normalize(dr, dim=-1)
         
-        return {'m': self.LJ.force(abs_dr) * unit_dr}
+        return {self.messageName: self.LJ.force(abs_dr) * unit_dr}
         
-    def f(self, t, g, derivativeName='v'):
-        g.update_all(self.calc_message, fn.sum('m', derivativeName))
+    def f(self, t, g, dynamicName=None, derivativeName=None):
+        self.set_dynamicName(dynamicName)
+        self.set_aggregateName(derivativeName)
+        g.update_all(self.calc_message, fn.sum(self.messageName, self.set_aggregateName))
         return g
 
     
@@ -57,7 +74,7 @@ class dr_nonPeriodic(nn.Module):
     def __init__(self):
         super().__init__()
         
-    def calc(self, r1, r2):
+    def forward(self, r1, r2):
         return r2 - r1
     
 class dr_periodic(nn.Module):
@@ -66,7 +83,7 @@ class dr_periodic(nn.Module):
         
         self.periodic = torch.tensor(periodic, dtype=torch.float32)
         
-    def calc(self, r1, r2):
+    def forward(self, r1, r2):
         dr = torch.remainder(r2 - r1, self.periodic)
         return dr - (dr > self.periodic/2) * self.periodic
     
@@ -111,13 +128,14 @@ class edgeCalculator(nn.Module):
         else:
             self.def_noSelfLoop()
             
+            
         
     def forward(self, r):
-        dr = self.distanceCalc.calc(torch.unsqueeze(r, 0), torch.unsqueeze(r, 1))
+        dr = self.distanceCalc(torch.unsqueeze(r, 0), torch.unsqueeze(r, 1))
         dr = torch.norm(dr, dim=-1)
         return self.distance2edge(dr)        
     
     
-class dynamicGODEwrapper(mo.dynamicGNDEmodule):
+LJ_GODEwrapper(mo.dynamicGNDEmodule):
     def __init__(self, calc_module, edgeConditionFunc, forceUpdate=False)
         super().__init__(calc_module, edgeConditionFunc, forceUpdate)
