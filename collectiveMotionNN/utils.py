e714fc81695262150c3c46a1992e8b7dbda123d7
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-03-30 12:43:43 +0900

    Update utils.py

--- collectiveMotionNN/utils.py
+++ collectiveMotionNN/utils.py
@@ -1,4 +1,5 @@
-
+import torch
+from torch import nn
 
 def variableInitializer(val, defaultVal):
     if val is None:
@@ -6,3 +7,22 @@ def variableInitializer(val, defaultVal):
     else:
         return val
 
+    
+class dr_nonPeriodic(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, r1, r2):
+        return r2 - r1
+    
+class dr_periodic(nn.Module):
+    def __init__(self, periodic):
+        super().__init__()
+        
+        self.periodic = torch.tensor(periodic, dtype=torch.float32)
+        
+    def forward(self, r1, r2):
+        dr = torch.remainder(r2 - r1, self.periodic)
+        return dr - (dr > self.periodic/2) * self.periodic
+    
+    
