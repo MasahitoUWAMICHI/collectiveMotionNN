9450eb38b82488e2b17d8dbe0cc557f51bcc4f50
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-04-09 13:45:41 +0900

    Update graph_utils.py

--- collectiveMotionNN/graph_utils.py
+++ collectiveMotionNN/graph_utils.py
@@ -47,7 +47,7 @@ def edgeRefresh_execute(gr, dynamicVariable, ndataInOutModule, edgeConditionModu
     return updateFunc(gr, edgeConditionModule, args)
 
     
-class edgeRefresh_forceUpdate(nn.Module):
+class edgeRefresh(nn.Module):
     def __init__(self, edgeConditionModule, returnScore=None, scorePostProcessModule=None, scoreIntegrationModule=None, forceUpdate=None, rtol=None, atol=None, equal_nan=None):
         super().__init__()
 
@@ -147,43 +147,7 @@ class edgeRefresh_forceUpdate(nn.Module):
 
 
     
-class singleVariableNdataInOut(nn.Module):
-    def __init__(self, variableName):
-        super().__init__()
-        
-        self.variableName = variableName
-    
-    def input(self, gr, variableValue):
-        gr.ndata[self.variableName] = variableValue
-        return gr
 
-    def output(self, gr):
-        return gr.ndata[self.variableName]
-    
-class multiVariableNdataInOut(nn.Module):
-    def __init__(self, variableName, variableNDims):
-        super().__init__()
-        
-        assert len(variableName) == len(variableNDims)
-        
-        self.variableName = variableName
-        self.variableNDims = variableNDims
-        
-        self.initializeIndices()
-        
-    def initializeIndices(self):
-        self.variableIndices = np.cumsum(np.array([0]+list(self.variableNDims), dtype=int))
-    
-    def input(self, gr, variableValue):
-        for vN, vD0, vD1 in zip(self.variableName, self.variableIndices[:-1], self.variableIndices[1:]):
-            gr.ndata[vN] = variableValue[..., vD0:vD1]
-        return gr
-
-    def output(self, gr):
-        return torch.cat([gr.ndata[vN] for vN in self.variableName], dim=-1)
-    
-    
-    
     
 
 def make_disconnectedGraph(dynamicVariable, ndataInOutModule, staticVariables=None):
@@ -233,53 +197,5 @@ class distance2edge_noSelfLoop(nn.Module):
 
     
     
-class radiusgraphEdge(nn.Module):
-    def __init__(self, r0, periodicLength=None, selfLoop=False, variableName=None):
-        super().__init__()
-           
-        self.r0 = r0
 
-        self.periodicLength = periodicLength
-        
-        self.selfLoop = selfLoop
-        
-        self.edgeVariable = ut.variableInitializer(variableName, 'x')
-        
-        self.def_dr()
-        
-        self.def_distance2edge()
-        
-        
-    def def_nonPeriodic(self):
-        self.distanceCalc = ut.euclidDistance_nonPeriodic()
-        
-    def def_periodic(self):
-        self.distanceCalc = ut.euclidDistance_periodic(self.periodicLength)
-        
-    def def_dr(self):
-        if self.periodicLength is None:
-            self.def_nonPeriodic()
-        else:
-            self.def_periodic()
-        
-
-    def def_noSelfLoop(self):
-        self.distance2edge = distance2edge_noSelfLoop(self.r0)
-        
-    def def_selfLoop(self):
-        self.distance2edge = distance2edge_selfLoop(self.r0)
-        
-    def def_distance2edge(self):
-        if self.selfLoop:
-            self.def_selfLoop()
-        else:
-            self.def_noSelfLoop()
-            
-            
-        
-    def forward(self, g, args=None):
-        dr = self.distanceCalc(torch.unsqueeze(g.ndata[self.edgeVariable], 0), torch.unsqueeze(g.ndata[self.edgeVariable], 1))
-        dr = torch.norm(dr, dim=-1, keepdim=False)
-        return self.distance2edge(dr)            
-    
     
