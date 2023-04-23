0e3ff8d3f6644381898262b76b853a844f2241fb
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-04-23 21:50:50 +0900

    Update springPotentialModel.py

--- collectiveMotionNN/examples/springPotentialModel.py
+++ collectiveMotionNN/examples/springPotentialModel.py
@@ -154,6 +154,7 @@ class interactionModule_nonParametric_acceleration(interactionModule):
         
         for key in self.fNN.state_dict().keys():
             eval(initFunc_prefix + key + initFunc_surfix)
+            print(key, ' requires_grad = ', self.fNN.state_dict()[key].requires_grad)
             self.fNN.state_dict()[key].register_hook(lambda grad: print(key+' grad', grad))
         
     def calc_message(self, edges):
