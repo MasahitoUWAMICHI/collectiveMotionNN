4d027896844c209fb73b1a1c41ffe3c87a5b8852
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-07-07 13:31:45 +0900

    Update springPotentialModel_utils.py

--- collectiveMotionNN/examples/springPotentialModel_utils.py
+++ collectiveMotionNN/examples/springPotentialModel_utils.py
@@ -63,7 +63,7 @@ def run_SDEsimulate(SDEwrapper, x0, t_save, dt_step, device, method_SDE, bm_levy
     return y
 
 
-def run_ODEsimulate(neuralDE, SDEwrapper, graph, x_truth, device, useScore=False):
+def run_ODEsimulate(neuralDE, SDEwrapper, graph, x_truth, device, t_learn_span, t_learn_save, useScore=False):
     torch.cuda.empty_cache()
     
     x_truth = x_truth.reshape([-1, x_truth.shape[-1]]).to(device)
