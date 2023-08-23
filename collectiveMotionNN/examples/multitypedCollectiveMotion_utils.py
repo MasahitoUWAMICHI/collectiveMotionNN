eb7a0b2fd482c20984a7da9321752e60bb87d5e3
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-08-23 16:46:28 +0900

    Update multitypedCollectiveMotion_utils.py

--- collectiveMotionNN/examples/multitypedCollectiveMotion_utils.py
+++ collectiveMotionNN/examples/multitypedCollectiveMotion_utils.py
@@ -67,7 +67,7 @@ def run_SDEsimulate(SDEwrapper, x0, t_save, dt_step, N_batch, N_particles, devic
     y[..., N_dim] = torch.remainder(y[..., N_dim], 2*np.pi)
     y = y.reshape((t_save.shape[0], N_batch, N_particles, 2*Nd-1))
     
-    ct = SDEwrapper.graph.ndata['celltype']
+    ct = SDEwrapper.graph.ndata['celltype'].detach().cpu()
     ct = ct.reshape((N_batch, N_particles, 1))
 
     return {'xtheta': y, 'celltype': ct}
