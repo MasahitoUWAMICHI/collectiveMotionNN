6cea937e2be544f2c934a2249cc9749bf6e403b2
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-08-23 15:46:04 +0900

    Update multitypedCollectiveMotionFunctions.py

--- collectiveMotionNN/examples/multitypedCollectiveMotionFunctions.py
+++ collectiveMotionNN/examples/multitypedCollectiveMotionFunctions.py
@@ -239,6 +239,89 @@ class interactionModule(nn.Module):
         return g
 
 
+class thetaLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, x, y):
+        return 1 - torch.mean(torch.cos(x - y))
+
+class myLoss(nn.Module):
+    def __init__(self, distanceCalc, N_dim=2, useScore=True):
+        super().__init__()
+        
+        self.distanceCalc = distanceCalc
+        self.useScore = useScore
+                
+        self.xyLoss = nn.MSELoss()
+        self.vLoss = nn.MSELoss()
+        
+        self.N_dim = N_dim
+        
+        self.def_forward()
+        
+    def forward_score(self, x, y, score_x, score_y):
+        dxy = self.distanceCalc(x[...,:self.N_dim], y[...,:self.N_dim])
+        xyLoss = self.xyLoss(dxy, torch.zeros_like(dxy))
+        vLoss = self.xyLoss(x[...,self.N_dim:(2*self.N_dim)], y[...,self.N_dim:(2*self.N_dim)])
+        scoreLoss = torch.mean(torch.square(torch.sum(score_x, dim=-1, keepdim=True) - score_y))
+        return xyLoss, vLoss, scoreLoss
+       
+    def forward_noScore(self, x, y):
+        dxy = self.distanceCalc(x[...,:self.N_dim], y[...,:self.N_dim])
+        xyLoss = self.xyLoss(dxy, torch.zeros_like(dxy))
+        vLoss = self.xyLoss(x[...,self.N_dim:(2*self.N_dim)], y[...,self.N_dim:(2*self.N_dim)])
+        return xyLoss, vLoss
+       
+    def def_forward(self):
+        if self.useScore:
+            self.forward = self.forward_score
+        else:
+            self.forward = self.forward_noScore
+        
+     
+class myLoss_normalized(nn.Module):
+    def __init__(self, distanceCalc, N_dim=2, useScore=True):
+        super().__init__()
+        
+        self.distanceCalc = distanceCalc
+        self.useScore = useScore
+                
+        self.xyLoss = nn.MSELoss()
+        self.vLoss = nn.MSELoss()
+        
+        self.N_dim = N_dim
+        
+        self.def_forward()
+        
+    def forward_score(self, x_pred, x_truth, score_pred, score_truth):
+        dxy = self.distanceCalc(x_pred[...,:self.N_dim], x_truth[...,:self.N_dim])
+        xyLoss = self.xyLoss(dxy, torch.zeros_like(dxy))
+        vLoss = self.xyLoss(x_pred[...,self.N_dim:(2*self.N_dim)], x_truth[...,self.N_dim:(2*self.N_dim)])
+        scoreLoss = torch.mean(torch.square(torch.sum(score_pred, dim=-1, keepdim=True) - score_truth))
+
+        x_truth_zero = self.distanceCalc(torch.zeros_like(x_pred[...,:self.N_dim]), x_truth[...,:self.N_dim])
+        xyNorm = self.xyLoss(x_truth_zero, torch.zeros_like(dxy))
+        vNorm = self.xyLoss(torch.zeros_like(x_pred[...,self.N_dim:(2*self.N_dim)]), x_truth[...,self.N_dim:(2*self.N_dim)])
+        scoreNorm = torch.mean(torch.square(score_truth))
+        return xyLoss/xyNorm, vLoss/vNorm, scoreLoss/scoreNorm
+       
+    def forward_noScore(self, x_pred, x_truth):
+        dxy = self.distanceCalc(x_pred[...,:self.N_dim], x_truth[...,:self.N_dim])
+        xyLoss = self.xyLoss(dxy, torch.zeros_like(dxy))
+        vLoss = self.xyLoss(x_pred[...,self.N_dim:(2*self.N_dim)], x_truth[...,self.N_dim:(2*self.N_dim)])
+
+        x_truth_zero = self.distanceCalc(torch.zeros_like(x_pred[...,:self.N_dim]), x_truth[...,:self.N_dim])
+        xyNorm = self.xyLoss(x_truth_zero, torch.zeros_like(dxy))
+        vNorm = self.xyLoss(torch.zeros_like(x_pred[...,self.N_dim:(2*self.N_dim)]), x_truth[...,self.N_dim:(2*self.N_dim)])
+        return xyLoss/xyNorm, vLoss/vNorm
+        
+    def def_forward(self):
+        if self.useScore:
+            self.forward = self.forward_score
+        else:
+            self.forward = self.forward_noScore     
+
 
 
 
