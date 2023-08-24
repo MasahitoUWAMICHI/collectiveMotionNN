39675007f94bd65246f1f9d7ebdb2477c4d12d0d
Masahito Uwamichi <88299450+MasahitoUWAMICHI@users.noreply.github.com>
2023-08-25 00:41:31 +0900

    Update multitypedCollectiveMotion_utils.py

--- collectiveMotionNN/examples/multitypedCollectiveMotion_utils.py
+++ collectiveMotionNN/examples/multitypedCollectiveMotion_utils.py
@@ -58,7 +58,8 @@ def run_SDEsimulate(SDEwrapper, x0, t_save, dt_step, N_batch, N_particles, devic
     bm = BrownianInterval(t0=t_save[0], t1=t_save[-1], 
                           size=(x0.shape[0], Nd-1), dt=dt_step, levy_area_approximation=bm_levy, device=device)
 
-    print(SDEwrapper.state_dict())
+    for keys in SDEwrapper.state_dict().keys():
+        print(key, SDEwrapper.state_dict()[key].dtype)
 
     with torch.no_grad():
         y = sdeint(SDEwrapper, x0.to(device), t_save, bm=bm, dt=dt_step, method=method_SDE)
